d74a64aab50395d818467b4893f217232f330de5
andreadotti <adotti@slac.stanford.edu>
2016-12-26 00:26:15 -0800

    Enabling material groups XS
    Enabling production of XS by group of materials.

--- runme.ProcessTest.xs.sh
+++ runme.ProcessTest.xs.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 set -e
 
+#Check if all or part of materials are expected
+if [ $# -ge 1 ];then
+   segment=$1
+   if [ $# -ge 2 ];then
+      totsegms=$1
+   else
+      totsegms=10
+   fi
+fi
 echo "Running ProcessTest version: "`ProcessTest --version`" in: "`pwd`
 
+cd crosssections
 echo "EM Cross-sections"
-./doXS_EM.sh
+./doXS_EM.sh $totsegms $segment
 echo "Had Cross-sections"
-./doXS_had.sh
+./doXS_had.sh $totsegms $segment
 
 #Copy output files
-tar czf output.tgz *.log *.json
+mkidr -p /output
+tar czf /output/crosssections${segment}.tgz *.log *.json
 
 #Copy to final location
